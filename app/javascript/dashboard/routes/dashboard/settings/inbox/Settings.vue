db5a252d1e5d118574dd468fe9e20a675b6856e2
Clinton Jarvis <clinton.jarvis@uceasy.ca>
2026-05-19 19:08:43 -0600

    fix: move custom attributes settings to inbox-settings tab

--- app/javascript/dashboard/routes/dashboard/settings/inbox/Settings.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/Settings.vue
@@ -1222,6 +1222,8 @@ export default {
               />
             </SettingsAccordion>
 
+            <CustomAttributesSettings :inbox="inbox" />
+
             <div class="w-full flex justify-end items-center py-4 mt-2">
               <NextButton
                 v-if="isAPIInbox"
@@ -1275,7 +1277,6 @@ export default {
           :class="isAWebWidgetInbox ? 'max-w-7xl' : 'max-w-4xl'"
         >
           <ConfigurationPage :inbox="inbox" />
-          <CustomAttributesSettings :inbox="inbox" />
         </div>
         <div
           v-if="selectedTabKey === 'voice-configuration'"
