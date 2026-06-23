5a6bd20cfa233b59d0d2e0e72ac885ed053861df
Clinton Jarvis <clinton.jarvis@uceasy.ca>
2026-06-23 10:27:15 -0600

    fix(auth): remove double URL-encoding of email in SSO callback
    ERB::Util.url_encode was applied to the email before passing it to
    login_page_url, which then calls .to_query (which also URL-encodes).
    This caused the @ symbol to become %2540 instead of %40, preventing
    SSO login from matching the user's email.

--- app/controllers/devise_overrides/omniauth_callbacks_controller.rb
+++ app/controllers/devise_overrides/omniauth_callbacks_controller.rb
@@ -20,8 +20,7 @@ class DeviseOverrides::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCa
     # once the resource is found and verified
     # we can just send them to the login page again with the SSO params
     # that will log them in
-    encoded_email = ERB::Util.url_encode(@resource.email)
-    redirect_to login_page_url(email: encoded_email, sso_auth_token: @resource.generate_sso_auth_token)
+    redirect_to login_page_url(email: @resource.email, sso_auth_token: @resource.generate_sso_auth_token)
   end
 
   def sign_in_user_on_mobile
@@ -33,8 +32,7 @@ class DeviseOverrides::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCa
     # once the resource is found and verified
     # we can just send them to the login page again with the SSO params
     # that will log them in
-    encoded_email = ERB::Util.url_encode(@resource.email)
-    params = { email: encoded_email, sso_auth_token: @resource.generate_sso_auth_token }.to_query
+    params = { email: @resource.email, sso_auth_token: @resource.generate_sso_auth_token }.to_query
 
     mobile_deep_link_base = GlobalConfigService.load('MOBILE_DEEP_LINK_BASE', 'chatwootapp')
     redirect_to "#{mobile_deep_link_base}://auth/saml?#{params}", allow_other_host: true
